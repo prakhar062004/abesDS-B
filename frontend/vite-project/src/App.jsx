fbd98fd9f60781df0033edb43ce40b675d5f067a
prakhar062004 <prakharharnaiya@gmail.com>
2025-10-31 15:40:11 +0530

    react

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './component/Card.jsx'
+import Nav from './component/Nav.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Nav /> <br />
+     <Card /> <br />
+     <Card/><br />
+     <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/vite-project/src/component/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Nav.css'
+
+
+const nav   = () => {
+  return (
+    <div nav id='nav'>
+        <h1>Navigation Bar</h1>
+    </div>
+  )
+}
+
+export default nav  
